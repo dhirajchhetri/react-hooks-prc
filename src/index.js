1ba88d9818a0965a84f15bc7f0bb122a96b7569b
Lever-Technologies <dhiraj@levertechno.com>
2022-07-11 04:05:39 +0530

    added fetch hook

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 import React, { useEffect, useReducer, useRef, useState } from "react";
 import ReactDOM from "react-dom";
+import useFetch from "./useFetch";
 
-function App() {
- return (
-   <h1>Hello World</h1>
- )
+function App({login}) {
+  const {data, loading, error } = useFetch(`https://api.github.com/users/${login}`);
+
+   if(loading) return <h1>Loading...</h1>
+   if(error) return (
+      <pre> {JSON.stringify(error, null, 2)}</pre>
+    )
+  if(data) return (
+    <div>
+      <img src={data.avatar_url}  alt={data.login} style={{maxHeight:'10%'}}/>
+      {data.login && <p>{data.login}</p>}
+      {data.name && <p>{data.name}</p>}
+
+    </div>
+    )
+
 }
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App login={"dhirajchhetri"}/>
   </React.StrictMode>,
   document.getElementById("root")
 );
---
+++ src/useFetch.js
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react'
+
+function useFetch(uri) {
+    const [data, setData]= useState();
+    const [loading, setLoading] = useState(true);
+    const [error, setError]= useState();
+
+    useEffect(()=>{
+        if(!uri) return;
+        fetch(uri)
+        .then((data)=> data.json())
+        .then(setData)
+        .then(()=>setLoading(false))
+        .catch( setError)
+
+    },[uri])
+  return {data, loading, error}
+}
+
+export default useFetch
