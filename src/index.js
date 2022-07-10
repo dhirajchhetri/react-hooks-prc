75729c147ba7da709c10133336ebfe9c5de81ff7
Lever-Technologies <dhiraj@levertechno.com>
2022-07-11 03:21:02 +0530

    reducer implementation

--- src/index.js
+++ src/index.js
@@ -2,8 +2,29 @@ import React, { useEffect, useReducer, useRef, useState } from "react";
 import ReactDOM from "react-dom";
 
 function App() {
+  const initialState={
+    message:"Hello"
+  }
+  const reducer = (state, action)=>{
+    switch (action.type){
+      case "whisper":
+        return {
+          message:'ssh!!!'
+        }
+      case "loud":
+        return {
+          message:"dont shout !!!"
+        }
+    }
+
+  }
+  const [state, dispatch] = useReducer(reducer, initialState)
  return (
-   <h1>Hello World</h1>
+   <>
+   <p>{state.message}</p>
+   <button onClick={()=> dispatch({type:"whisper"})}> Whisper</button>
+   <button onClick={()=> dispatch({type:"loud"})}>loud</button>
+   </>
  )
 }
 
